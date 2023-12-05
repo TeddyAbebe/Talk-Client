0ea0240acb8cb6451bb993a565312545ef0481ee
Tewodros Abebe <teddyab729@gmail.com>
2023-12-05 08:06:33 +0300

    Home Page

---
+++ src/Components/Authentication/SignUp.js
@@ -0,0 +1,16 @@
+import { FormControl, FormLabel, Input, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <VStack spacing="5px">
+        <FormControl>
+            <FormLabel>
+                <Input pla/>
+            </FormLabel>
+        </FormControl>
+    </VStack>
+  )
+}
+
+export default SignUp
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../Components/Authentication/Login";
+import SignUp from "../Components/Authentication/SignUp";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        textAlign="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="DM-Sans" color="black">
+          Talk
+        </Text>
+      </Box>
 
-export default HomePage
+      <Box
+        bg={"white"}
+        w={"100%"}
+        p={4}
+        color="black"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel><Login /></TabPanel>
+            <TabPanel><SignUp /></TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
